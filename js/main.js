5acc82306164efc32bd24dc5b28acfcfafde5411
pripaula2002 <61168280+pripaula2002@users.noreply.github.com>
2023-04-07 20:43:29 -0300

    Novas linhas de codigo

--- js/main.js
+++ js/main.js
@@ -13,7 +13,21 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
+ import swURL from 'sw:../service-worker.js';
 
+ // Register the service worker
+ if ('serviceWorker' in navigator) {
+   // Wait for the 'load' event to not block other work
+   window.addEventListener('load', async () => {
+     // Try to register the service worker.
+     try {
+       const reg = await navigator.serviceWorker.register(swURL);
+       console.log('Service worker registered! 😎', reg);
+     } catch (err) {
+       console.log('😥 Service worker registration failed: ', err);
+     }
+   });
+ }
 window.addEventListener('DOMContentLoaded', async () => {
   // Set up the editor
   const { Editor } = await import('./app/editor.js');
